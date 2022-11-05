223d87c794f4191f9d1fec0f838d9cb6b3f9fd2f
Samuel Overington <s.c.overington@gmail.com>
2022-11-05 09:53:57 +0100

    testing page + chart base init

---
+++ components/Chart.js
@@ -0,0 +1,49 @@
+// components/Chart.js
+import React from 'react'
+import { Bar } from 'react-chartjs-2'
+
+export default function(props) {
+
+  return (
+    <Bar
+      data={parseData(props.data)}
+      width={50}
+      height={100}
+      options={options}
+      />
+  )
+}
+
+const parseData = data => ({
+  labels: ['Dogs %', 'Cats %', 'Hamsters %'],
+    datasets: [
+      {
+        label: 'The polls awakening',
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)'
+        ],
+        borderWidth: 5,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data
+      }
+    ]
+})
+  
+const options = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  },
+  maintainAspectRatio: false
+}
