9ccb8c86ad2bf9419922d3e3632aceaa2cb21937
Samuel Overington <s.c.overington@gmail.com>
2024-11-16 11:28:18 +0000

    admin set question fix bux

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -4,8 +4,6 @@ import useSWR from "swr";
 import { API_URL } from "@/data/config";
 
 
-
-
 // fetcher function to get the current question
 async function fetcher_GET_current_question(url: string) {
   const response = await fetch(url);
@@ -29,6 +27,19 @@ async function fetcher_PUT_current_question(url: string, token: string) {
   }
   return response.json();
 }
+async function fetch_POST_current_question(url:string, token: string) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+}
 
 export function useCurrentQuestion(token: string) {
   
@@ -43,22 +54,8 @@ export function useCurrentQuestion(token: string) {
   const setCurrentQuestion = async (question_id: string) => {
     try {
       console.log("Changing question to:", question_id);
-      // const response = await fetch(`${API_URL}/?set_current=${question_id}`, {
-      //   method: "PUT",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     Authorization: `Bearer ${token}`,
-      //   },
-      // });
       const response = await fetcher_PUT_current_question(`${API_URL}/?set_current=${question_id}`, token);
-
-      if (!response.ok) {
-        console.log("Response:", response);
-        throw new Error("Network response was not ok");
-      }
-
-      const response_json = await response.json();
-      console.log("Question changed successfully:", response_json);
+      console.log("Question changed successfully:", response);
       mutate();
     } catch (error) {
       console.error("Failed to change question:", error);
@@ -66,23 +63,25 @@ export function useCurrentQuestion(token: string) {
   };
   const voteHandler = async (question_id: string, vote_idx: string) => {
     try {
-      const submit_url = `${API_URL}/?question_id=${question_id}&vote_idx=${vote_idx}`;
-      console.log("Submitting vote to:", submit_url);
-      const response = await fetch(submit_url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (!response.ok) {
-        console.log("Response:", response);
-        throw new Error("Network response was not ok");
-      }
-
-      const response_json = await response.json();
-      console.log("Vote submitted successfully:", response_json);
+      // const submit_url = `${API_URL}/?question_id=${question_id}&vote_idx=${vote_idx}`;
+      // console.log("Submitting vote to:", submit_url);
+      // const response = await fetch(submit_url, {
+        //   method: "POST",
+        //   headers: {
+          //     "Content-Type": "application/json",
+          //     Authorization: `Bearer ${token}`,
+          //   },
+          // });
+          
+          // if (!response.ok) {
+            //   console.log("Response:", response);
+            //   throw new Error("Network response was not ok");
+            // }
+            
+            // const response_json = await response.json();
+            // console.log("Vote submitted successfully:", response_json);
+            
+      const response = await fetch_POST_current_question(`${API_URL}/?question_id=${question_id}&vote_idx=${vote_idx}`, token);
       mutate();
     } catch (error) {
       console.error("Failed to submit vote:", error);
