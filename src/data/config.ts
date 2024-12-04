14b083a8c36f5dbf058b5294c0e4747d591e7c31
Samuel Overington <s.c.overington@gmail.com>
2024-12-04 21:10:24 +0100

    linting errors

--- src/data/config.ts
+++ src/data/config.ts
@@ -4,4 +4,4 @@ export const ADMIN_TOKEN = data["ADMIN"]["TOKEN"];
 export const USER_TOKEN = data["USER"]["TOKEN"];
 
 export const API_URL = "/vote"
-export const PROD_ADDRESS = "http://localhost:3000"
+export const PROD_ADDRESS = "http://172.16.2.254"
