d46347be9638ffdd9217645c7e83032d5cc2a89c
Samuel Overington <s.c.overington@gmail.com>
2024-12-06 19:12:32 +0100

    page layout padding

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 
 export default function InstructionsPage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="grid grid-cols-1 lg:grid-cols-2">
           <Card className="text-xl">
